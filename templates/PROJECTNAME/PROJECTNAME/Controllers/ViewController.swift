2c527a2f6526cf7bb718a17167dfb581c4703838
Carlos Santos <carlosricardosantos@gmail.com>
2018-07-21 12:02:43 +0100

    Added initial template folder

---
+++ templates/PROJECTNAME/PROJECTNAME/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PROJECTNAME
+//
+//  Created by Carlos Santos on 21/07/2018.
+//  Copyright © 2018 ORGANIZATIONNAME. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ templates/PROJECTNAME/PROJECTNAMETests/PROJECTNAMETests.swift
@@ -0,0 +1,34 @@
+//
+//  PROJECTNAMETests.swift
+//  PROJECTNAMETests
+//
+//  Created by Carlos Santos on 21/07/2018.
+//  Copyright © 2018 ORGANIZATIONNAME. All rights reserved.
+//
+
+import XCTest
+@testable import PROJECTNAME
+
+class PROJECTNAMETests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
